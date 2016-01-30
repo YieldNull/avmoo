22ffd5e2015f2697ac2f44ef334c57232ae78bf1
YieldNull <yieldnull@gmail.com>
2016-01-31 00:54:39 +0800

    use agents and proxies pool

--- avmoo.py
+++ avmoo.py
@@ -3,28 +3,35 @@
 """
 Copy javmoo.xyz
 """
-
+import random
 import re
+from time import sleep
 import requests
 from bs4 import BeautifulSoup
 import MySQLdb
 import _mysql_exceptions
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, current_process
 
 # URL以及正则pattern
-host = u'www.javmoo.xyz'
-server = u'www.javmoo.xyz/cn'
-ptn_server = u'www\.javmoo\.xyz/cn'
+host = 'www.javmoo.xyz'
+server = host + '/cn'
+ptn_server = server.replace('.', '\.')
 
-url_actresses = u'http://%s/actresses/currentPage/{}' % server
+# url
+url_actresses = 'http://%s/actresses/currentPage/{page}' % server
+url_movie_list = 'http://%s/star/{sid}/currentPage/{page}' % server
+url_movie_list_home = 'http://%s/star/{sid}' % server
+url_movie = 'http://%s/movie/{mid}' % server
+
+# 演员列表
 ptn_actresses = u'http://%s/star/(.*)' % ptn_server
 ptn_next_actresses = u'<a href="/cn/actresses/currentPage/(\d+)">下一页</a>'
 
-url_movies = u'http://%s/star/{}/currentPage/{}' % server
+# 作品列表
 ptn_movies = u'http://%s/movie/(.*)' % ptn_server
-ptn_next_movies = u'<a href="/cn/star/{}/currentPage/(\d+)">下一页</a>'
+ptn_next_movies = u'<a href="/cn/star/{sid}/currentPage/(\d+)">下一页</a>'
 
-url_movie = u'http://%s/movie/{}' % server
+# 作品详情
 ptn_movie_name = u'<h3>(.*?)</h3>'
 ptn_movie_vid = u'<span class="header">识别码:</span> <span style="color:#CC0000;">(.*?)</span>'
 ptn_movie_time = u'<p><span class="header">发行时间:</span>(.*?)</p>'
@@ -34,8 +41,9 @@ ptn_movie_cate = u'<a href="http://%s/genre/.*?">(.*?)</a>' % ptn_server
 ptn_movie_actor = u'<a class="avatar-box" href="http://%s/star/(.*?)">' % ptn_server
 ptn_movie_sample = u'<a class="sample-box" href="(.*?)">'
 
+# http headers
 headers = {
-    'Host': 'www.javmoo.xyz',
+    'Host': host,
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
@@ -47,8 +55,59 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
 }
 
+# http referer
+referer_movie_list_home = 'http://%s/actresses' % server
+referer_movie_list = 'http://%s/star/{sid}/currentPage/{page}' % server
+referer_movie = 'http://%s/star/{sid}' % server
+
+delay = 0.5
+
+# User-Agent Pool
+user_agents = []
+with open('agents.txt') as f:
+    for line in f:
+        user_agents.append(line.split())
+
+http_proxies = []
+with open('proxies.txt') as f:
+    for line in f:
+        l = line.split()
+        http_proxies.append('%s:%s' % (l[0], l[1]))
+
 # 数据库
 dbconn = MySQLdb.connect(user='root', passwd='19961020', db='avmoo', charset='utf8', use_unicode=True)
+
+
+def do_get(url, hds=headers, retry=0):
+    """
+    获取源码
+    :param url:
+    :param hds:
+    :param retry:
+    :return:
+    """
+    agent = random.choice(user_agents)
+    hds = hds.copy()
+    hds['User-Agent'] = agent
+
+    try:
+        if retry == 0:
+            res = requests.get(url, headers=hds, timeout=3)
+        elif retry <= 2:
+            res = requests.get(url, headers=headers, timeout=3, proxies={'http': 'lotusland.club:3128'})
+        else:
+            proxy = random.choice(http_proxies)
+            proxies = {'http': proxy}
+            res = requests.get(url, headers=hds, timeout=3, proxies=proxies)
+    except requests.exceptions.Timeout:
+        return do_get(url, headers, retry + 1)
+
+    code = res.status_code
+
+    if code != 200:
+        print '[%d] agent:%d retrying......' % (code, user_agents.index(agent))
+        return do_get(url, headers, retry + 1)
+    return res.text, code
 
 
 def get_star_list(page=1):
@@ -58,7 +117,9 @@ def get_star_list(page=1):
     :return: [{'sid':演员id,'name':演员姓名,'img':演员头像URL}],下一页的页码
             下一页页码为None则表示遍历结束
     """
-    res = requests.get(url_actresses.format(page), headers=headers).text
+    res, code = do_get(url_actresses.format(page=page))
+    print '[%d] page:%d' % (code, page)
+
     result = []
     soup = BeautifulSoup(res)
     items = soup.find_all('div', class_='item')
@@ -90,7 +151,16 @@ def get_movie_list(sid, page=1):
     :return: [{'mid':作品id,'img':作品封面URL}]，下一页的页码
             下一页页码为None则表示遍历结束
     """
-    res = requests.get(url_movies.format(sid, page), headers=headers).text
+    hd = headers.copy()
+    if page == 1:
+        hd['Referer'] = referer_movie_list_home
+        url = url_movie_list_home.format(sid=sid)
+    else:
+        hd['Referer'] = referer_movie_list.format(sid=sid, page=int(page) - 1)
+        url = url_movie_list.format(sid=sid, page=page)
+
+    res, code = do_get(url, hd)
+    print '[%d] sid:%s page:%s' % (code, sid, page)
 
     result = []
     soup = BeautifulSoup(res)
@@ -108,7 +178,7 @@ def get_movie_list(sid, page=1):
         img = item.img['src']
         result.append({'mid': mid, 'img': img})
 
-    match = re.search(ptn_next_movies.format(sid), res)
+    match = re.search(ptn_next_movies.format(sid=sid), res)
     if match:
         return result, match.group(1)
     else:
@@ -122,8 +192,10 @@ def get_movie(mid):
     :return: {'mid':影片id,'id': 番号, 'name': 名称, 'time': 发行时间, 'length': 时长（分钟）,
             'cover': 封面大图URL, 'cates': 分类列表, 'actors': 演员列表, 'samples': 样图列表}
     """
+    sleep(delay)
+    res, code = do_get(url_movie.format(mid=mid))
+    print '[%d] mid:%s' % (code, mid)
 
-    res = requests.get(url_movie.format(mid), headers=headers).text
     name = re.search(ptn_movie_name, res)
     name = name.group(1).strip() if name else ''
 
@@ -156,9 +228,12 @@ def fetch_star(page):
     try:
         stars, p = get_star_list(page)
     except requests.exceptions.ConnectionError:
-        print 'ConnectionError: page:%d' % page
+        print 'ConnectionError: fetch_star page:%d' % page
         return fetch_star(page)
     else:
+        leng = len(stars)
+        if leng == 0:
+            return fetch_star(page)
         print page, len(stars)
         return stars
 
@@ -175,8 +250,13 @@ def fetch_movie_id(sid):
         try:
             movies, page = get_movie_list(sid, page)
         except requests.exceptions.ConnectionError:
+            print 'ConnectionError: fetch_movie_id sid:%s' % sid
             continue
         movie_list += movies
+        leng = len(movies)
+        if leng == 0:
+            page -= 1
+            continue
         print sid, len(movies)
     return movie_list
 
@@ -233,7 +313,12 @@ def create_db():
     :return:
     """
     cursor = dbconn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS movie_actor')
+    cursor.execute('DROP TABLE IF EXISTS movie_sample')
+    cursor.execute('DROP TABLE IF EXISTS movie_cate')
     cursor.execute('DROP TABLE IF EXISTS movie')
+    cursor.execute('DROP TABLE IF EXISTS star')
+
     cursor.execute("""
         CREATE TABLE movie(
             mid     VARCHAR(127) PRIMARY KEY ,
@@ -245,7 +330,6 @@ def create_db():
             small   VARCHAR(127)
         )
     """)
-    cursor.execute('DROP TABLE IF EXISTS star')
     cursor.execute("""
         CREATE TABLE star(
             sid     VARCHAR(127) PRIMARY KEY ,
@@ -253,7 +337,6 @@ def create_db():
             img     VARCHAR(127)
         )
     """)
-    cursor.execute('DROP TABLE IF EXISTS movie_actor')
     cursor.execute("""
         CREATE TABLE movie_actor(
             mid     VARCHAR(127),
@@ -263,7 +346,6 @@ def create_db():
             FOREIGN KEY (sid) REFERENCES star(sid)
         )
     """)
-    cursor.execute('DROP TABLE IF EXISTS movie_sample')
     cursor.execute("""
         CREATE TABLE movie_sample(
             mid     VARCHAR(127),
@@ -272,7 +354,6 @@ def create_db():
             FOREIGN KEY (mid) REFERENCES movie(mid)
         )
     """)
-    cursor.execute('DROP TABLE IF EXISTS movie_cate')
     cursor.execute("""
         CREATE TABLE movie_cate(
             mid     VARCHAR(127),
@@ -294,7 +375,7 @@ def insert(sql, data):
     try:
         cursor.execute(sql, data)
     except _mysql_exceptions.IntegrityError:
-        print 'duplicated', data
+        print 'duplicated'
     finally:
         dbconn.commit()
         cursor.close()
@@ -415,6 +496,21 @@ def read_movie_id():
         movies.append(mid)
     cursor.close()
     return movies
+
+
+def serial():
+    for sid in read_star():
+        ml = fetch_movie_id(sid)
+        store_movie_id(ml)
+
+
+def test():
+    for sid in read_star():
+        page = 1
+        while page:
+            movie_list, page = get_movie_list(sid, page)
+            # for movie in movie_list:
+            #     get_movie(movie['mid'])
 
 
 if __name__ == '__main__':
